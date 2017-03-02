fd8aaf4a44467ef42f06041cee2e27fdc014f785
ntnt <trang.nguyen@whl.travel>
2017-03-02 17:41:12 +0700

    face

--- application/views/product.php
+++ application/views/product.php
@@ -171,6 +171,19 @@
         </div>
     </div>
 </div>
+<div class="row" style="text-align: center">
+
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1611276859129198";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+    <div class="fb-comments" data-href="http://thoitrangchobe.com.vn/Qun-o-b-gi-85" data-width="500" data-numposts="5"></div>
+</div>
+<div class="fb-like" data-href="http://thoitrangchobe.com.vn/Qun-o-b-gi-85" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
 <script>
     var size_by_color = '<?php echo json_encode($size_by_color);?>';
 </script>
